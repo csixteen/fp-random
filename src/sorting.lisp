7361aac5357d7bc8adbb78bdce34afedbbbff083
Pedro Rodrigues <csixteen@protonmail.com>
2020-09-06 09:06:33 +0100

    MergeSort ; bug fix

--- src/sorting.lisp
+++ src/sorting.lisp
@@ -1,9 +1,9 @@
 ;;; Insertion Sort
 
 (defun insert (elem lst)
-  (cond ((null lst) (list a))
-        ((> (car lst) a) (cons a lst))
-        (t (cons (car lst) (insert a (cdr lst))))))
+  (cond ((null lst) (list elem))
+        ((> (car lst) elem) (cons elem lst))
+        (t (cons (car lst) (insert elem (cdr lst))))))
 
 (defun insertion-sort (lst)
   (cond ((null lst) nil)
@@ -19,3 +19,28 @@
       (let ((lt (remove-if-not #'(lambda (y) (< y x)) xs))
             (ge (remove-if-not #'(lambda (y) (>= y x)) xs)))
         (append (quicksort lt) (list x) (quicksort ge))))))
+
+
+;;; Merge Sort
+
+(defun halve (lst)
+  "Splits a sequence in half and returns both
+  halves"
+  (let ((n (floor (/ (length lst) 2))))
+    (values (subseq lst 0 n)
+            (subseq lst n))))
+
+(defun simple-merge (xs ys)
+  (cond ((null xs) ys)
+        ((null ys) xs)
+        ((< (first xs) (first ys))
+         (cons (first xs) (simple-merge (rest xs) ys)))
+        (t (cons (first ys) (simple-merge xs (rest ys))))))
+
+(defun merge-sort (lst)
+  (cond ((null lst) nil)
+        ((null (rest lst)) lst)
+        (t (multiple-value-bind (left right)
+             (halve lst)
+             (simple-merge (merge-sort left)
+                           (merge-sort right))))))
